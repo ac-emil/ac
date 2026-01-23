67bd2fca4fec20bc110f4c67e97fbc8c2ea23ba2
ac-emil <emil@aestheticconversion.com>
2026-01-24 01:59:02 +0800

    Update ac-util.js

--- ac-util.js
+++ ac-util.js
@@ -1,3 +1,7 @@
+// TOGGLE ELEMENTS
+function toggleElementClass(e, t) { e ? e.classList.toggle(t) : console.error(`Element with selector '${e}' not found.`) } const elementsCanToggle = document.querySelectorAll(".toggle-element"); window.addEventListener("load", () => { elementsCanToggle.forEach(e => { const t = e.querySelector(".toggle-trigger"), l = e.querySelectorAll(".toggle-close-trigger"), o = e.querySelector(".toggle-content"); t.addEventListener("click", () => { elementsCanToggle.forEach(t => { if (t !== e && t.classList.contains("show")) { const e = t.querySelector(".toggle-content"); e.classList.toggle("show"), t.classList.toggle("show") } }), toggleElementClass(e, "show"), toggleElementClass(o, "show") }), l.forEach(t => { t.addEventListener("click", () => { toggleElementClass(e, "show"), toggleElementClass(o, "show") }) }) }) });
+
+
 // LENIS SMOOTH SCROLL
 let lenis;
 if (Webflow.env("editor") === undefined) {
@@ -38,3 +42,4 @@ const footerYear = document.querySelectorAll(".current-year");
 for (let i = 0; i < footerYear.length; i++) {
     footerYear[i].textContent = new Date().getFullYear();
 }
+
